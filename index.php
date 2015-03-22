9b1e05c28dac7a46a05e0af5a9803beec995a956
Michael Cunningham <michael.cunningham@live.ca>
2015-03-21 23:53:40 -0300

    Tweaked Page, enabled available demos

--- index.php
+++ index.php
@@ -77,7 +77,7 @@
 							<p>In computing, a denial-of-service (DoS) or distributed denial-of-service (DDoS) attack is an attempt to make a machine or network resource unavailable to its intended users.</p>
 						</div>
 						<div class="panel-footer">
-							<p><a class="btn btn-default disabled" href="#" role="button">View demonstration &raquo;</a></p>
+							<p><a class="btn btn-default" href="/denial-of-service" role="button">View demonstration &raquo;</a></p>
 						</div>
 					</div>
                 </div>
@@ -90,7 +90,7 @@
 							<p>Cross-site request forgery, also known as a one-click attack or session riding and abbreviated as CSRF (sometimes pronounced sea-surf) or XSRF, is a type of malicious exploit of a website whereby unauthorized commands are transmitted from a user that the website trusts.</p>
 						</div>
 						<div class="panel-footer">
-							<p><a class="btn btn-default disabled" href="#" role="button">View demonstration &raquo;</a></p>
+							<p><a class="btn btn-default" href="/cross-site-request-forgery" role="button">View demonstration &raquo;</a></p>
 						</div>
 					</div>
                 </div>
@@ -105,20 +105,20 @@
 							<p>A data breach is the intentional or unintentional release of secure information to an untrusted environment. Other terms for this phenomenon include unintentional information disclosure, data leak and also data spill.</p>
 						</div>
 						<div class="panel-footer">
-							<p><a class="btn btn-default disabled" href="#" role="button">View demonstration &raquo;</a></p>
+							<p><a class="btn btn-default" href="/data-breach" role="button">View demonstration &raquo;</a></p>
 						</div>
 					</div>
                 </div>
                 <div class="col-md-4">
 					<div class="panel panel-default">
 						<div class="panel-heading">
-							<h4>Arbitrary file inclusion</h4>
+							<h4>Malware Distribution</h4>
 						</div>
 						<div class="panel-body">
-							<p>File inclusion vulnerability is a type of vulnerability most often found on websites. It allows an attacker to include a file, usually through a script on the web server.</p>
+							<p>A Trojan Horse is a program that uses malicious code masqueraded as a trusted application. The malicious code can be injected on benign applications, masqueraded in e-mail links, or sometimes hidden in JavaScript pages to make furtive attacks against vulnerable internet Browsers.</p>
 						</div>
 						<div class="panel-footer">
-							<p><a class="btn btn-default disabled" href="#" role="button">View demonstration &raquo;</a></p>
+							<p><a class="btn btn-default" href="/malware-distribution" role="button">View demonstration &raquo;</a></p>
 						</div>
 					</div>
                 </div>
@@ -131,7 +131,7 @@
 							<p>Local File Inclusion (also known as LFI) is the process of including files, that are already locally present on the server, through the exploiting of vulnerable inclusion procedures implemented in the application.</p>
 						</div>
 						<div class="panel-footer">
-							<p><a class="btn btn-default disabled" href="#" role="button">View demonstration &raquo;</a></p>
+							<p><a class="btn btn-default disabled" href="/local-file-inclusion" role="button">View demonstration &raquo;</a></p>
 						</div>
 					</div>
                 </div>
@@ -146,7 +146,7 @@
 							<p>Remote File Include (RFI) is an attack technique used to exploit "dynamic file include" mechanisms in web applications. When web applications take user input (URL, parameter value, etc.) and pass them into file include commands, the web application might be tricked into including remote files with malicious code.</p>
 						</div>
 						<div class="panel-footer">
-							<p><a class="btn btn-default disabled" href="#" role="button">View demonstration &raquo;</a></p>
+							<p><a class="btn btn-default disabled" href="/remote-file-inclusion" role="button">View demonstration &raquo;</a></p>
 						</div>
 					</div>
                 </div>
@@ -159,20 +159,20 @@
 							<p>In computer security and programming, a buffer overflow, or buffer overrun, is an anomaly where a program, while writing data to a buffer, overruns the buffer's boundary and overwrites adjacent memory. This is a special case of violation of memory safety.</p>
 						</div>
 						<div class="panel-footer">
-							<p><a class="btn btn-default disabled" href="#" role="button">View demonstration &raquo;</a></p>
+							<p><a class="btn btn-default disabled" href="/buffer-overflow" role="button">View demonstration &raquo;</a></p>
 						</div>
 					</div>
                 </div>
                 <div class="col-md-4">
 					<div class="panel panel-default">
 						<div class="panel-heading">
-							<h4>Other code injection</h4>
+							<h4>Memory corruption</h4>
 						</div>
 						<div class="panel-body">
-							<p>Code injection is the exploitation of a computer bug that is caused by processing invalid data. Code injection can be used by an attacker to introduce (or "inject") code into a computer program to change the course of execution. The results of a code injection attack can be disastrous. For instance, code injection is used by some computer worms to propagate.</p>
+							<p>Referencing memory after it has been freed can cause a program to crash. The use of heap allocated memory after it has been freed or deleted leads to undefined system behavior</p>
 						</div>
 						<div class="panel-footer">
-							<p><a class="btn btn-default disabled" href="#" role="button">View demonstration &raquo;</a></p>
+							<p><a class="btn btn-default" href="/memory-corruption" role="button">View demonstration &raquo;</a></p>
 						</div>
 					</div>
                 </div>
